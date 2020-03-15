53f72268d0506852977bd28f86457205d14cae5b
Elias <eliasbrucart@hardgames.com.ar>
2020-03-15 03:00:35 -0300

    POST now return keys in json

--- server/server.js
+++ server/server.js
@@ -4,17 +4,20 @@ const app = express();
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get('/usuario', function(req, res) {
     res.json('get Usuario')
 });
 
 app.post('/usuario', function(req, res) {
-    res.json('post Usuario')
+    let body = req.body;
+    res.json({
+        persona: body
+    })
 });
 
 app.put('/usuario/:id', function(req, res) {
