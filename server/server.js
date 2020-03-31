7b0a3f80a0c7793c4458169fd0ad5cd81edd4e25
Elias <eliasbrucart@hardgames.com.ar>
2020-03-31 04:22:07 -0300

    Orderred user routes

--- server/server.js
+++ server/server.js
@@ -3,7 +3,11 @@ require('./config/config.js');
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+
 const bodyParser = require('body-parser');
+
+app.use(require('./routes/usuario.js'));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,33 +15,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
-});
+mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-});
+    if (err) throw err;
 
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
+    console.log('Base de dato ONLINE');
 
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
 });
 
 
